34dca64b823b2b4bb51be624c4e575a0c10a14f8
John <aleksandr@giftexpress.ru>
2022-12-28 18:24:03 +0300

    App finished

--- app/src/main/java/com/example/plantshandbook/MainActivity.kt
+++ app/src/main/java/com/example/plantshandbook/MainActivity.kt
@@ -2,16 +2,41 @@ package com.example.plantshandbook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.plantshandbook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val adapter = PlantAdapter()
+    private val imageIdList = listOf(
+        R.drawable.plant1,
+        R.drawable.plant2,
+        R.drawable.plant3,
+        R.drawable.plant4,
+        R.drawable.plant5
+    )
+
+    private var idx = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        init()
 
+    }
 
+    private fun init() {
+        binding.apply {
+            rcView.layoutManager = GridLayoutManager(this@MainActivity, 2)
+            rcView.adapter = adapter
+            btAdd.setOnClickListener {
+                if (idx > 4) idx = 0
+                val plant = Plant(imageIdList[idx], "Plant $idx")
+                adapter.addPlant(plant)
+                idx++
+            }
+        }
     }
 }
